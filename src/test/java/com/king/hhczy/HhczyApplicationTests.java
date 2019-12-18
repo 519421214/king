d73268ebf6dca10a9c9cc9faf145be581e897ed1
ningjinxiang <519421214@qq.com>
2019-12-18 11:28:58 +0800

    微服务搭建

---
+++ src/test/java/com/king/hhczy/HhczyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.king.hhczy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhczyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
